55a3ac063de3b87a8529b23452a8debfdfe49a90
hai061898 <truonghaiauto98@gmail.com>
2021-11-28 15:39:56 +0700

    database

---
+++ src/database/database.js
@@ -0,0 +1,26 @@
+//link mysql: https://github.com/hai061898/mysql_food
+
+import mysql from 'mysql';
+import { promisify } from 'util';
+
+const pool = mysql.createPool({
+    host: 'localhost:8080',
+    user: 'root',
+    password: "",
+    database: 'food'
+});
+
+pool.getConnection((err,connection) => {
+    if(err){
+        if(err.code === 'PROTOCOL_CONNECTION_LOST')  console.log('DATABASE CONNECTION WAS CLOSED');
+        if( err.code === 'ER_CON_COUNT_ERROR' ) console.log('DATABASE HAS TO MANY CONNECTIONS');
+        if( err.code === 'ECONNREFUSED' ) console.log('DATABASE CONNECTION WAS REFUSED');
+    }
+    if(connection) connection.release(); // giống tự đông kết nối lại 
+    console.log('DataBase is connected to '+ process.env.DB_DATABASE);
+    return;
+});
+
+pool.query = promisify(pool.query);
+
+export default pool;
