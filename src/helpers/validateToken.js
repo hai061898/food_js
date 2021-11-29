22af067de1f791c70c485d367ed78f876c12b8c0
hai061898 <truonghaiauto98@gmail.com>
2021-11-29 21:31:42 +0700

    helper

--- src/helpers/validateToken.js
+++ src/helpers/validateToken.js
@@ -1,9 +1,25 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-export default ValidatedToken = (req, res ,next) => {
-    let token = req.header('xx-token');
-   
-    if(!token){
+export default ValidatedToken = (req, res, next) => {
+  let token = req.header("xx-token");
+  // thuật toán
 
-    }
-}
+  if (!token) {
+    return res.status(401).json({
+      resp: false,
+      msg: "There is not Token in the request",
+    });
+  }
+  try {
+    const { uidPerson } = jwt.verify(token, process.env.APP_KEY_JWT);
+    // mã hóa
+    req.uidPerson = uidPerson;
+    next();
+    //  tính logic 
+  } catch (err) {
+    return res.status(401).json({
+      resp: false,
+      msg: err,
+    });
+  }
+};
